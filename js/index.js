6841b945be6a8d2529fa870679fd492825bfba05
Gabriel Assana <gabrielhhz2@gmail.com>
2020-07-06 22:43:17 -0300

    Consumo de Api do Back Adicionado

--- js/index.js
+++ js/index.js
@@ -7,22 +7,37 @@ async function getRepos(event){
   
   event.preventDefault();
   
- const urlFront = ("https://api.github.com/repos/frontendbr/vagas/issues")
+ const urlFront = "https://api.github.com/repos/frontendbr/vagas/issues"
+ const urlBack =  "https://api.github.com/repos/backend-br/vagas/issues"
+ let url = ""
+
+ let select = document.getElementById("search");
+ let text = select.options[select.selectedIndex].text
+
+  if(text == "Front-end"){      
+    url = urlFront
+  };
+
+  if(text == "Back-end"){
+    url = urlBack
+  };
  
- let response = await fetch(urlFront);
+ let response = await fetch(url);
  let result = await response.json();
+
  
- result.forEach(element =>{
-      const card = `
+
+ result.forEach(element =>{       
+  
+   const vaga = `
       <ul class= "vagas">
         <li class= "titulo-vagas">Vaga: ${element.title}</li>    
         <li>Descricao: ${element.body}</li>
         <br>     
       </ul>
   `;
-  
-    document.querySelector('#sessao-vagas').innerHTML += card; 
-
-     console.log(result);
+    
+    document.querySelector('#sessao-vagas').innerHTML += vaga;
+    
   });
 };
