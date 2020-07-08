8608b0d6768ef1fab076434fbe665ab2f81b95b2
Gabriel Assana <gabrielhhz2@gmail.com>
2020-07-08 20:47:41 -0300

    Alterações Finais  API e pgs de cadastro e login

--- js/index.js
+++ js/index.js
@@ -3,40 +3,39 @@ const btnSearch = document.querySelector(".btn-search");
 btnSearch.addEventListener("click", getRepos);
 
 
-async function getRepos(event){
-  
-  event.preventDefault();
-  
- const urlFront = "https://api.github.com/repos/frontendbr/vagas/issues"
- const urlBack =  "https://api.github.com/repos/backend-br/vagas/issues"
- let url = ""
+async function getRepos(event) {
 
- let select = document.getElementById("search");
- let text = select.options[select.selectedIndex].text
- let vaga =""
+    event.preventDefault();
 
-  if(text == "Front-end"){      
-    url = urlFront
-  };
+    const urlFront = "https://api.github.com/repos/frontendbr/vagas/issues"
+    const urlBack = "https://api.github.com/repos/backend-br/vagas/issues"
+    let url = ""
 
-  if(text == "Back-end"){
-    url = urlBack
-  };
- 
- let response = await fetch(url);
- let result = await response.json();
+    let select = document.getElementById("search");
+    let text = select.options[select.selectedIndex].text
+    let sessaoVaga = document.querySelector('#sessao-vagas');
+    sessaoVaga.innerHTML = "";
 
- result.forEach(element =>{       
-  
-   vaga = `
+    if (text === "Front-end") {
+        url = urlFront;
+    } else if (text === "Back-end") {
+        url = urlBack;
+    }
+
+    let response = await fetch(url);
+    let result = await response.json();
+
+    result.forEach(element => {
+
+        const vaga = `
       <ul class= "vagas">
         <li class= "titulo-vagas">Vaga: ${element.title}</li>    
         <li class= "desc-vagas">Descricao: ${element.body}</li>
         <br>     
       </ul>
   `;
-    
-    document.querySelector('#sessao-vagas').innerHTML += vaga;
-    
-  });
+
+        sessaoVaga.innerHTML += vaga;
+
+    });
 };
